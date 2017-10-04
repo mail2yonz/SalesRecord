07250b178febbd11911ef1970ea13e53d0e18e26
GraphicStarr <mail2yonz@gmail.com>
2017-10-04 09:14:47 -0400

    sales calculation commit

---
+++ src/Sales.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Sales {
+
+
+   public static void main(String[] args)
+   {
+
+      int Customer_Id;
+      String Customer_Name,TaxCode;
+      double SalesAmount,TotalAmount,TaxAmount=0.00;
+
+      Scanner keyboard = new Scanner(System.in);
+
+
+      System.out.println("Enter Customer's ID: ");
+      Customer_Id=keyboard.nextInt();
+
+      System.out.println("Enter Customer's Name: ");
+      Customer_Name=keyboard.next();
+
+
+      System.out.println("Enter Customer's TaxCode: ");
+      TaxCode=keyboard.next();
+
+
+      System.out.println("Enter Customer's SalesAmount: ");
+      SalesAmount=keyboard.nextDouble();
+
+      if(TaxCode.equalsIgnoreCase ( "NRM" ))
+      {
+         TaxAmount=0.06;
+      }else if(TaxCode.equalsIgnoreCase ( "NPF" ))
+      {
+         TaxAmount=0.00;
+      }else if(TaxCode.equalsIgnoreCase ( "BIZ" ))
+      {
+         TaxAmount=0.045;
+      }
+
+       TotalAmount=SalesAmount + SalesAmount*TaxAmount;
+
+      System.out.println("The Customer ID is :"+ Customer_Id);
+      System.out.println("The Customers Name is :" + Customer_Name);
+      System.out.println("The TaxCode is: :"+ TaxCode);
+      System.out.println("The  SalesAmount is :"+ SalesAmount);
+      System.out.println("The  TaxAmount is :"+ TaxAmount);
+      System.out.println("The total Amount is :"+ TotalAmount);
+
+
+
+   }
+
+
+
+
+
+
+
+}
